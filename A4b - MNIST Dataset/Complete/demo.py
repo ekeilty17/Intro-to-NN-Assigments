4db8ae1de0da10b1894ac1f9fb2eaec03ff36a35
Eric <epkeilty@hotmail.com>
2020-05-18 23:57:52 -0400

    created handwritting demo

--- A4b - MNIST Dataset/Complete/demo.py
+++ A4b - MNIST Dataset/Complete/demo.py
@@ -1,3 +1,8 @@
+
+"""
+    Skip to the next multi-line comment
+"""
+
 # conda install -c anaconda tk
 import tkinter as tk
 
@@ -16,60 +21,45 @@ def get_handwritten_digit(ROWS, COLS):
         col = int(event.x // col_width)
         row = int(event.y // row_height)
         
-        # If the tile is not filled, create a rectangle
-        if tiles[row][col] != 4:
-            c.create_rectangle(col*col_width, row*row_height, (col+1)*col_width, (row+1)*row_height, fill="white")
-            tiles[row][col] = 4
+        # If the tile is not filled nor previously filled, create a rectangle
+        if tiles[row][col] is None or tiles[row][col][0] != 4:
             
-            # This is adding grey edges, this helps the model correctly classify your digit
+            # this kinda gets messy beause we need to store both the tile value and the reference to the tile
+            #   the tile value is used for our model's prediction
+            #   the tile reference is used so we can delete tiles if needed (this is returned by c.create_rectangle())
+            tiles[row][col] = (4, c.create_rectangle(col*col_width, row*row_height, (col+1)*col_width, (row+1)*row_height, fill="white"))
+            
+            # This is adding grey edges, which helps the model correctly classify your digit
+            
+            #   orthogonal tiles
             try:
-                if tiles[row-1][col] is None:
-                    tiles[row-1][col] = 2
-                    c.create_rectangle(col*col_width, (row-1)*row_height, (col+1)*col_width, (row)*row_height, fill="light grey")
-            except:
-                pass
-            try:
-                if tiles[row+1][col] is None:
-                    tiles[row+1][col] = 2
-                    c.create_rectangle(col*col_width, (row+1)*row_height, (col+1)*col_width, (row+2)*row_height, fill="light grey")
-            except:
-                pass
-            try:
-                if tiles[row][col-1] is None:
-                    tiles[row][col-1] = 2
-                    c.create_rectangle((col-1)*col_width, row*row_height, (col)*col_width, (row+1)*row_height, fill="light grey")
-            except:
-                pass
-            try:
-                if tiles[row][col+1] is None:
-                    tiles[row][col+1] = 2
-                    c.create_rectangle((col+1)*col_width, row*row_height, (col+2)*col_width, (row+1)*row_height, fill="light grey")
+                if tiles[row-1][col] is None or tiles[row-1][col][0] == 1:
+                    tiles[row-1][col] = (2, c.create_rectangle(col*col_width, (row-1)*row_height, (col+1)*col_width, (row)*row_height, fill="light grey"))
+                
+                if tiles[row+1][col] is None or tiles[row-1][col][0] == 1:
+                    tiles[row+1][col] = (2, c.create_rectangle(col*col_width, (row+1)*row_height, (col+1)*col_width, (row+2)*row_height, fill="light grey"))
+                
+                if tiles[row][col-1] is None or tiles[row-1][col][0] == 1:
+                    tiles[row][col-1] = (2, c.create_rectangle((col-1)*col_width, row*row_height, (col)*col_width, (row+1)*row_height, fill="light grey"))
+
+                if tiles[row][col+1] is None or tiles[row-1][col][0] == 1:
+                    tiles[row][col+1] = (2, c.create_rectangle((col+1)*col_width, row*row_height, (col+2)*col_width, (row+1)*row_height, fill="light grey"))
             except:
                 pass
             
-            # diagonals
+            #   diagonals tiles
             try:
                 if tiles[row-1][col-1] is None:
-                    tiles[row-1][col-1] = 1
-                    c.create_rectangle((col-1)*col_width, (row-1)*row_height, (col)*col_width, (row)*row_height, fill="grey")
-            except:
-                pass
-            try:
+                    tiles[row-1][col-1] = (1, c.create_rectangle((col-1)*col_width, (row-1)*row_height, (col)*col_width, (row)*row_height, fill="grey"))
+
                 if tiles[row+1][col+1] is None:
-                    tiles[row+1][col+1] = 1
-                    c.create_rectangle((col+1)*col_width, (row+1)*row_height, (col+2)*col_width, (row+2)*row_height, fill="grey")
-            except:
-                pass
-            try:
+                    tiles[row+1][col+1] = (1, c.create_rectangle((col+1)*col_width, (row+1)*row_height, (col+2)*col_width, (row+2)*row_height, fill="grey"))
+
                 if tiles[row+1][col-1] is None:
-                    tiles[row+1][col-1] = 1
-                    c.create_rectangle((col-1)*col_width, (row+1)*row_height, (col)*col_width, (row+2)*row_height, fill="grey")
-            except:
-                pass
-            try:
+                    tiles[row+1][col-1] = (1, c.create_rectangle((col-1)*col_width, (row+1)*row_height, (col)*col_width, (row+2)*row_height, fill="grey"))
+
                 if tiles[row-1][col+1] is None:
-                    tiles[row-1][col+1] = 1
-                    c.create_rectangle((col+1)*col_width, (row-1)*row_height, (col+2)*col_width, (row)*row_height, fill="grey")
+                    tiles[row-1][col+1] = (1, c.create_rectangle((col+1)*col_width, (row-1)*row_height, (col+2)*col_width, (row)*row_height, fill="grey"))
             except:
                 pass
 
@@ -84,8 +74,8 @@ def get_handwritten_digit(ROWS, COLS):
         row = int(event.y // row_height)
         
         # If the tile is not filled, create a rectangle
-        if tiles[row][col]:
-            c.delete(tiles[row][col])
+        if not tiles[row][col] is None:
+            c.delete(tiles[row][col][1])
             tiles[row][col] = None
 
     # Create the window, a canvas and the mouse click event binding
@@ -95,13 +85,13 @@ def get_handwritten_digit(ROWS, COLS):
     # these bind the buttom presses for different actions
     c.pack()
     c.bind("<B1-Motion>", draw)         # if the mouse is clicked and in motion, it will draw
-    c.bind("<ButtonPress-1>", delete)   # if you just click once, you can remove cells
+    c.bind("<Button-1>", delete)   # if you just click once, you can remove cells
 
     # starts the main loop, which is waiting for an event to trigger the callback functions
     root.mainloop() 
 
     # returns the bitmap, which is used as the input to our model
-    bmp = [ [0 if x is None else x for x in row] for row in tiles ]
+    bmp = [ [0 if t is None else t[0] for t in row] for row in tiles ]
     return np.array(bmp)
 
 
@@ -120,7 +110,8 @@ def predict(model, image):
     image = torch.tensor(image).unsqueeze(0).unsqueeze(0)       # need to add a dimension for the batch
                                                                 # and another for the channel
     prediction = model(image.float())
-    prediction_probs = torch.nn.Softmax()(prediction)
+    prediction_probs = torch.nn.Softmax()(prediction)           # you technically don't need to do this, 
+                                                                # but it's nice to have them a probabilities
     return prediction_probs.squeeze().data.numpy()
 
 def display_prediction(prediction):
